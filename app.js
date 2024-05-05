a333cda55246fa61afc4591762ede7adfa97b39a
A Beetle <abeetle15@gmail.com>
2024-05-05 13:38:50 -0500

    Add eraser and draw.
    create userColors array that initially only contains the default
    black color. when user changes color the new color gets pushed to
    userColors to keep track. when user stops erasing and presses draw btn
    it goes to the second to last color in userColors to return.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // const all the elemnts
 const pad = document.querySelector('.pad');
+const drawBtn = document.querySelector('.drawBtn');
+const eraseBtn = document.querySelector('.eraseBtn');
 const resizeBtn = document.querySelector('.resizeBtn');
 const clearBtn = document.querySelector('.clearBtn');
 const blackColor = document.querySelector('.black')
@@ -7,46 +9,52 @@ const redColor = document.querySelector('.red')
 const greenColor = document.querySelector('.green')
 const blueColor = document.querySelector('.blue')
 const yellowColor = document.querySelector('.yellow');
-let globPix;
-
 
 const black = 'rgb(9, 9, 9)';
 const red = 'rgb(247, 5, 22)';
 const green = 'rgb(63, 173, 50)';
 const blue = '#2279CE';
 const yellow = '#FDF124';
+const white = 'rgb(248, 248, 246)';
 
+let globPix;
+let userColors = [black];
 let color = black;
 let mouseFlag = false;
+
 let pixels = document.querySelectorAll('.pixel');
 
-clearBtn.addEventListener('click', () => {
+drawBtn.addEventListener('click', e => { prevColor() })
+
+eraseBtn.addEventListener('click', e => { eraser() })
+
+clearBtn.addEventListener('click', e => {
   clear();
 });
 
-blackColor.addEventListener('click', () => {
+blackColor.addEventListener('click', e => {
   changeColor(black)
 
 })
-redColor.addEventListener('click', () => {
+redColor.addEventListener('click', e => {
   changeColor(red)
 })
-greenColor.addEventListener('click', () => {
+greenColor.addEventListener('click', e => {
   changeColor(green)
 })
-blueColor.addEventListener('click', () => {
+blueColor.addEventListener('click', e => {
   changeColor(blue)
 })
-yellowColor.addEventListener('click', () => {
+yellowColor.addEventListener('click', e => {
   changeColor(yellow)
 })
 
 
-resizeBtn.addEventListener('click', () => {
+resizeBtn.addEventListener('click', e => {
   resize()
 });
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', e => {
   createGrid(60);
   adEv();
 })
@@ -65,33 +73,28 @@ function createGrid(resolution) {
 }
 
 function adEv() {
-  pad.addEventListener('mousedown', handler);
+  pad.addEventListener('click', () => {
+    let pixels = document.querySelectorAll('.pixel');
+    globPix = pixels;
+
+    // toggle a flag to decide when to paint based on click
+    if (!mouseFlag) {
+      mouseFlag = true;
+
+    } else if (mouseFlag) {
+      mouseFlag = false;
+
+    }
+    // when user wants to paint every div mouse enter gets colored
+    pixels.forEach((pixel) => {
+      pixel.addEventListener('mouseenter', paint);
+    })
+  });
 }
 
-function handler() {
-
-  let pixels = document.querySelectorAll('.pixel');
-  globPix = pixels;
-
-
-  // Check Mouse
-  if (!mouseFlag) {
-    mouseFlag = true;
-
-  } else if (mouseFlag) {
-    mouseFlag = false;
-
-  }
-  // pixelEvent
-  pixels.forEach((pixel) => {
-    pixel.addEventListener('mouseenter', paint);
-  })
-}
 
 function paint(event) {
   if (mouseFlag) {
-
-
     event.target.style.backgroundColor = color;
   } else {
 
@@ -100,7 +103,18 @@ function paint(event) {
 
 function changeColor(choice) {
   color = choice;
+  userColors.push(color);
 }
+
+function eraser() {
+  changeColor(white);
+}
+
+function prevColor() {
+  if (userColors.length > 1) {
+    color = userColors[(userColors.length - 2)];
+  }
+};
 
 function resize() {
   pad.innerHTML = '';
